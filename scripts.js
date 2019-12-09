81dc3bff6dc891dcb81b3c6e6683bd6e6ee1392c
Linda Eng <skyboxx@gmail.com>
2019-12-09 12:07:35 -0500

    on off toggle

--- scripts.js
+++ scripts.js
@@ -1,61 +1,92 @@
 //Google chrome won't allow sound to play unless there is a user interaction.
-document.querySelector('button').addEventListener('click', function() {
-  Tone.context.resume().then(() => {
-    var synth = new Tone.Synth().toMaster();
+// document.getElementById('on').addEventListener('click', function(e) {
+// 	// Tone.Transport.start();
 
-    
-    synth.triggerAttackRelease('C4', '8n');
+// document.getElementById('play').innerText = "Pause"
 
-    const synths = [
-    new Tone.Synth(),
-    new Tone.Synth(),
-    new Tone.Synth()
-    ];
+ 
 
+//   if(e.target.innerText==="Turn On") {
+//   	Tone.Transport.start();
+// 		playSynths();
+// 		document.getElementById('on').innerText="Activated"
 
+// 	} 
+// 	else {
+// 	}
+// });
 
-    synths[0].oscillator.type = 'triangle';
-    synths[1].oscillator.type = 'sine';
-    synths[2].oscillator.type = 'sawtooth';
+document.getElementById('play').addEventListener('click', function(e) {
+	// Tone.Transport.start();
 
-    const gain = new Tone.Gain(0.6);
-    gain.toMaster();
+	if(e.target.innerText==="Click Turn On To Start"){
+		  	Tone.Transport.start();
+		playSynths();
+		document.getElementById('on').innerText="Activated"
+				document.getElementById('play').innerText="Pause"
+	}
 
-    synths.forEach(synth => synth.connect(gain));
-
-    const $rows = document.body.querySelectorAll('div > div'); 
-    const notes =['G5','E4','C3'];
-    let index = 0;
-
-
-    Tone.Transport.scheduleRepeat(repeat, '8n'); 
-
-    Tone.context.resume();
-    Tone.Transport.start();
-    Tone.context.resume();
-
-
-    function repeat(time) {
-      let step = index % 8; //the columns
-
-      for (let i = 0; i < $rows.length; i++) {
-        let synth = synths[i]; //finds out synth
-        let note = notes[i]; // finds our note
-        let $row = $rows[i]; //finds our row
-        let $input = $row.querySelector(`input:nth-child(${step+1}`); //nth selectors start with 1
-        
-        if ($input.checked) {
-          synth.triggerAttackRelease(note, '8n', time);
-        }
-      }
-
-      index ++;
-    }
-
-    console.log('Playback resumed successfully');
-  });
+	else if(e.target.innerText==="Pause"){
+		console.log("pause")
+		document.getElementById('play').innerText="Play";
+		Tone.Transport.pause();
+	}
+	else if(e.target.innerText==="Play"){
+		console.log("playing")
+		document.getElementById('play').innerText="Pause"
+		Tone.Transport.start();
+	}
 
 });
+
+ 
+
+function playSynths(){
+	var synth = new Tone.Synth().toMaster();	
+	synth.triggerAttackRelease('C4', '8n');
+
+	const synths = [
+		new Tone.Synth(),
+		new Tone.Synth(),
+		new Tone.Synth()
+	];
+
+	synths[0].oscillator.type = 'triangle';
+	synths[1].oscillator.type = 'sine';
+	synths[2].oscillator.type = 'sawtooth';
+
+	const gain = new Tone.Gain(0.6);
+	gain.toMaster();
+	synths.forEach(synth => synth.connect(gain));
+
+
+	let index = 0;
+	const $rows = document.body.querySelectorAll('div > div'); 
+	const notes = ['G5','E4','C3'];
+
+	function repeat(time) {
+		let step = index % 8; //the columns
+
+		for (let i = 0; i < $rows.length; i++) {
+			let synth = synths[i]; //finds out synth
+			let note = notes[i]; // finds our note
+			let $row = $rows[i]; //finds our row
+			let $input = $row.querySelector(`input:nth-child(${step+1}`); //nth selectors start with 1
+			
+			if ($input.checked) {
+				synth.triggerAttackRelease(note, '8n', time);
+			}
+		}
+		index ++;
+	}
+
+	Tone.Transport.scheduleRepeat(repeat, '8n');
+	console.log('Playback resumed successfully');
+}
+
+
+
+
 
 
 
